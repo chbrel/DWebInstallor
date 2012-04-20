2a059d16d32c8e7f9f492e5250d182d31ae5977e
Christian Brel <ch.brel@gmail.com>
2012-04-20 19:51:11 +0200

    First version of web installor

---
+++ app/models/HelpUtils.java
@@ -0,0 +1,78 @@
+package models;
+
+public class HelpUtils {
+
+	public static String HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+		+ "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n"
+		+ "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n"
+		+ "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\" lang=\"fr\">\n"
+		+ "  <head>\n"
+		+ "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n"
+		+ "    <title>CD DeViNT: Aide sur les jeux</title>\n"
+		+ "    <link rel=\"shortcut icon\" href=\"./resources/aide.ico\" type=\"image/x-icon\" />\n"
+		+ "    <link rel=\"icon\" href=\"./resources/aide.ico\" type=\"image/x-icon\" />\n"
+		+ "    <style type=\"text/css\" media=\"all\">@import \"./css/base.css\";</style>\n"
+		+ "    <style type=\"text/css\" media=\"all\">@import \"./css/aide.css\";</style>\n"
+		+ "  </head>\n"
+		+ "  <body>\n"
+		+ "    <div id=\"banner\">\n"
+		+ "      <h1 id=\"title\">\n"
+		+ "	<a href=\"./index.html\"><img height=\"60\" alt=\"DeViNT\" id=\"logo\" src=\"./resources/logoCDDeViNT.png\" /></a>\n"
+		+ "	Aide du CD DeViNT</h1>\n"
+		+ "    </div>\n"
+		+ "      <ul class=\"navbar\" id=\"menu\">\n"
+		+ "    <li><span class=\"hideme\"><a href=\"#skip\" accesskey=\"s\" title=\"Passer le menu de navigation pour aller à la partie principale du document\">Passer la Navigation</a> |</span>\n"
+		+ "    <a href=\"./index.html\" accesskey=\"h\" title=\"CD DeViNT: Aide\"><strong>Accueil</strong></a></li>\n"
+		+ "    <li><a href=\"./jeux.html\" title=\"Aide sur les Jeux Installés\">Jeux Installés</a></li>\n"
+		+ "    <li><a href=\"./installation.html\" title=\"Installation du CD\">Installation Du CD</a></li>\n"
+		+ "    <li><a href=\"./api.html\" title=\"Comment créer son propre projet DeViNT\">Créer Son Projet</a></li>\n"
+		+ "    <li><a href=\"./os.html\" title=\"Jeux et Système D'Exploitation\">Systèmes D'Exploitation</a></li>\n"
+		+ "    <li><a href=\"./faq.html\" title=\"Foire aux questions\">Foire Aux Questions</a></li>\n"
+		+ "    <li><a href=\"./contact.html\" title=\"Nous Contacter\">Nous Contacter</a></li>\n"
+		+ "</ul>\n"
+		+ "    <div id=\"main\">\n"
+		+ "      <a name=\"skip\"></a>\n"
+		+ "		 <a name=\"top\" ></a>\n";
+	
+	public static String HEADER_INGAMEFOLDER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+		+ "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n"
+		+ "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n"
+		+ "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\" lang=\"fr\">\n"
+		+ "  <head>\n"
+		+ "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n"
+		+ "    <title>CD DeViNT: Aide sur les jeux</title>\n"
+		+ "    <link rel=\"shortcut icon\" href=\"../resources/aide.ico\" type=\"image/x-icon\" />\n"
+		+ "    <link rel=\"icon\" href=\"../resources/aide.ico\" type=\"image/x-icon\" />\n"
+		+ "    <style type=\"text/css\" media=\"all\">@import \"../css/base.css\";</style>\n"
+		+ "    <style type=\"text/css\" media=\"all\">@import \"../css/aide.css\";</style>\n"
+		+ "  </head>\n"
+		+ "  <body>\n"
+		+ "    <div id=\"banner\">\n"
+		+ "      <h1 id=\"title\">\n"
+		+ "	<a href=\"../index.html\"><img height=\"60\" alt=\"DeViNT\" id=\"logo\" src=\"../resources/logoCDDeViNT.png\" /></a>\n"
+		+ "	Aide du CD DeViNT</h1>\n"
+		+ "    </div>\n"
+		+ "      <ul class=\"navbar\" id=\"menu\">\n"
+		+ "    <li><span class=\"hideme\"><a href=\"#skip\" accesskey=\"s\" title=\"Passer le menu de navigation pour aller à la partie principale du document\">Passer la Navigation</a> |</span>\n"
+		+ "    <a href=\"../index.html\" accesskey=\"h\" title=\"CD DeViNT: Aide\"><strong>Accueil</strong></a></li>\n"
+		+ "    <li><a href=\"../jeux.html\" title=\"Aide sur les Jeux Installés\">Jeux Installés</a></li>\n"
+		+ "    <li><a href=\"../installation.html\" title=\"Installation du CD\">Installation Du CD</a></li>\n"
+		+ "    <li><a href=\"../api.html\" title=\"Comment créer son propre projet DeViNT\">Créer Son Projet</a></li>\n"
+		+ "    <li><a href=\"../os.html\" title=\"Jeux et Système D'Exploitation\">Systèmes D'Exploitation</a></li>\n"
+		+ "    <li><a href=\"../faq.html\" title=\"Foire aux questions\">Foire Aux Questions</a></li>\n"
+		+ "    <li><a href=\"../contact.html\" title=\"Nous Contacter\">Nous Contacter</a></li>\n"
+		+ "</ul>\n"
+		+ "    <div id=\"main\">\n"
+		+ "      <a name=\"skip\"></a>\n"
+		+ "		 <a name=\"top\" ></a>\n";
+	
+	public static String FOOTER = "<div class=\"clear\">&nbsp;</div>\n" +
+			"		<a href=\"#top\">Haut de page</a>" +
+			"	</div>\n" +
+			"  </body>\n" +
+			"</html>\n";
+	
+	public static String GAMELIST = "";
+	
+	public static String GAMELIST_INGAMEFOLDER = "";
+}
